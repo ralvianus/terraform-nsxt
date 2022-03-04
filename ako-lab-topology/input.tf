0024d6db0991bb25ff7dc07182ca761ab2a93ccf
Robert Alvianus <robert@alvianus.net>
2022-03-04 12:05:01 +0800

    update..

--- ako-lab-topology/input.tf
+++ ako-lab-topology/input.tf
@@ -1,8 +1,3 @@
 variable "nsxt_host"	{}
 variable "nsxt_username"	{}
 variable "nsxt_password"	{}
-
-# NSX-T parameters
-nsxt_host                  = "172.16.10.117"
-nsxt_username              = "admin"
-nsxt_password              = "VMware1!SDDC"
