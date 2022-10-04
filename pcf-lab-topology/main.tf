ba87636a0ff8415523b285559e8e6b5474494ed2
Robert Alvianus <robert@alvianus.net>
2022-10-04 22:44:36 +0800

    Update main.tf

--- pcf-lab-topology/main.tf
+++ pcf-lab-topology/main.tf
@@ -64,6 +64,12 @@ resource "nsxt_policy_tier1_gateway" "t1_avi" {
   pool_allocation           = "ROUTING"
 }
 
+resource "nsxt_policy_tier1_gateway" "t1_tas" {
+  nsx_id                    = "T1-Router-TAS-Deployment"
+  display_name              = "T1-Router-TAS-Deployment"
+  dhcp_config_path          = data.nsxt_policy_dhcp_server.dhcp-server.path
+}
+
 # Creating Segments
 
 resource "nsxt_policy_segment" "avi-mgmt-00" {
